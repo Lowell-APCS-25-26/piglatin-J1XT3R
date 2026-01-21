ebc9c79960dbc2aad281d7ff3d0d9fdb185e1d95
J1XT3R <80366820+J1XT3R@users.noreply.github.com>
2026-01-21 03:58:15 -0800

    Yay

--- lib/src/main/java/org/APCSLowell/PigLatin.java
+++ lib/src/main/java/org/APCSLowell/PigLatin.java
@@ -4,14 +4,55 @@ public class PigLatin {
     public int findFirstVowel(String sWord) {
         //precondition: sWord is a valid String of length greater than 0.
         //postcondition: returns the position of the first vowel in sWord.  If there are no vowels, returns -1
-	    
+	    String vowels = "AEIOUaeiou";
+
+        for (int i = 0; i < sWord.length(); i++) {
+            if (vowels.indexOf(sWord.charAt(i)) != -1) {
+                return i;
+            }
+
+            if (i > 0 && (sWord.charAt(i) == 'y' || sWord.charAt(i) == 'Y')) {
+                return i;
+            }
+        }
+
         return -1;
     }
 
     public String pigLatin(String sWord) {
         //precondition: sWord is a valid String of length greater than 0
         //postcondition: returns the pig latin equivalent of sWord
+        int firstVowel = findFirstVowel(sWord);
         
-        return null;
+        if (firstVowel == 0) {
+            return sWord + "way";
+        }
+        
+        if (sWord.length() > 1 && sWord.substring(0, 2).toLowerCase().equals("qu")) {
+            return sWord.substring(2) + sWord.substring(0, 2) + "ay";
+        }
+        
+        // Special case: if the only "vowel" is 'y' and it's not at the beginning,
+        // treat the whole word as having no vowels
+        if (firstVowel != -1) {
+            boolean hasRealVowel = false;
+            String realVowels = "AEIOUaeiou";
+            for (int i = 0; i < sWord.length(); i++) {
+                if (realVowels.indexOf(sWord.charAt(i)) != -1) {
+                    hasRealVowel = true;
+                    break;
+                }
+            }
+            // If no real vowels and only 'y' found, treat as no vowels
+            if (!hasRealVowel && (sWord.charAt(firstVowel) == 'y' || sWord.charAt(firstVowel) == 'Y')) {
+                return sWord + "ay";
+            }
+        }
+        
+        if (firstVowel == -1) {
+            return sWord + "ay";
+        }
+        
+        return sWord.substring(firstVowel) + sWord.substring(0, firstVowel) + "ay";
     }
 }
